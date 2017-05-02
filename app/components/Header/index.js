964f1c02a256db5c58c502b16af095f59d57a253
Davey <davey@myiworlds.com>
2017-05-01 22:13:06 -0700

    Implement FlatButton with Link for Material ui.  FormattedMessage?
    Might keep Formatted message because it actually turns the tet and numbers into the language of the user.  Need to figure out if Material ui has this or implement my own component coping the styles of Material-UI and adding it.

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -4,9 +4,11 @@ import { FormattedMessage } from 'react-intl';
 import A from './A';
 import Img from './Img';
 import NavBar from './NavBar';
+import { Link } from 'react-router';
 import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
 import messages from './messages';
+import FlatButton from 'material-ui/FlatButton';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -16,6 +18,8 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
           <Img src={Banner} alt="react-boilerplate - Logo" />
         </A>
         <NavBar>
+          <FlatButton containerElement={<Link to={'/'} />} label={messages.home.defaultMessage} />
+          <FlatButton containerElement={<Link to={'/Features'} />} label={messages.features.defaultMessage} />
           <HeaderLink to="/">
             <FormattedMessage {...messages.home} />
           </HeaderLink>
