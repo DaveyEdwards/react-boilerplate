07d9ab8b2527ce860f4b28c2ddc86676974a7818
Davey <davey@myiworlds.com>
2017-05-02 21:21:21 -0700

    Revert Header back to default
    Going to have to start breaking apart everything.  But, next action is database with GraphQL, Relay!
    Copy relay-fullstack's setup where it is in file, then make it on datastore

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,39 +1,31 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import A from './A';
 import Img from './Img';
 import NavBar from './NavBar';
-import { Link } from 'react-router';
 import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
 import messages from './messages';
-import FlatButton from 'material-ui/FlatButton';
-import { injectIntl, intlShape } from 'react-intl';
 
-const Header = ({ intl }) => { // eslint-disable-line react/prefer-stateless-function
-  const testbutton = intl.formatMessage(messages.home)
-  return (
-    <div>
-      <A href="https://twitter.com/mxstbr">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      <NavBar>
-        <FlatButton containerElement={<Link to={'/'} />} label={testbutton} />
-        <FlatButton containerElement={<Link to={'/Features'} />} label={messages.features.defaultMessage} />
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
-    </div>
-  );
+class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <A href="https://twitter.com/mxstbr">
+          <Img src={Banner} alt="react-boilerplate - Logo" />
+        </A>
+        <NavBar>
+          <HeaderLink to="/">
+            <FormattedMessage {...messages.home} />
+          </HeaderLink>
+          <HeaderLink to="/features">
+            <FormattedMessage {...messages.features} />
+          </HeaderLink>
+        </NavBar>
+      </div>
+    );
+  }
 }
 
-Header.propTypes = {
-  intl: intlShape.isRequired
-};
-
-export default injectIntl(Header);
+export default Header;
